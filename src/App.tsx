c995b4b42ebf13e0bd7bb93fa77d9d4063af8925
Antonio <antonio.ibarra99@outlook.es>
2026-05-14 13:30:38 -0700

    Welcome added

--- src/App.tsx
+++ src/App.tsx
@@ -59,24 +59,53 @@ function App() {
               className="-mt-28 w-175 h-175"
             />
             <div className="w-full flex flex-col justify-center items-center gap-4 -mt-28">
-              <div className="w-full">1</div>
               <div className="flex justify-evenly items-center w-full">
                 <div className="flex flex-col justify-center items-center gap-2 p-4 bg-[#171717] border border-[#262626] rounded-xl">
-                  <p className="text-[#D5D3D3] font-bold">Factory Info.</p>
+                  <div className="flex justify-evenly items-center w-full">
+                    {/* <div className="bg-[#D70B60] rounded-xl p-2 w-10 h-10 flex justify-center items-center">
+                      <i className="bi bi-router text-xl"></i>
+                    </div> */}
+                    <p className="text-[#D5D3D3] font-bold">Factory Info.</p>
+                  </div>
                   <p className="text-[#949A9C]">
-                    Get the factory details <br /> of the product
+                    Get the factory details of the <br />
+                    product
                   </p>
                 </div>
                 <div className="flex flex-col justify-center items-center gap-2 p-4 bg-[#171717] border border-[#262626] rounded-xl">
-                  <p className="text-[#D5D3D3] font-bold">Product Defect</p>
+                  <div className="flex justify-evenly items-center w-full">
+                    {/*  <div className="bg-[#FF6A32] rounded-xl p-2 w-10 h-10 flex justify-center items-center">
+                      <i className="bi bi-exclamation-triangle text-xl"></i>
+                    </div> */}
+                    <p className="text-[#D5D3D3] font-bold">Product Defect</p>
+                  </div>
                   <p className="text-[#949A9C]">
-                    Get the defect <br /> associated to that product
+                    Get the defect associated to that <br />
+                    product
                   </p>
                 </div>
                 <div className="flex flex-col justify-center items-center gap-2 p-4 bg-[#171717] border border-[#262626] rounded-xl">
-                  <p className="text-[#D5D3D3] font-bold">Time Line</p>
+                  <div className="flex justify-center items-center gap-2 w-full">
+                    {/* <div className="bg-[#0076DF] rounded-xl p-2 w-10 h-10 flex justify-center items-center">
+                      <i className="bi bi-clock text-xl"></i>
+                    </div> */}
+                    <p className="text-[#D5D3D3] font-bold">Time Line</p>
+                  </div>
                   <p className="text-[#949A9C]">
-                    Get the time line of this <br /> product in the process{" "}
+                    Get the time line of this product in <br />
+                    the process{" "}
+                  </p>
+                </div>
+                <div className="flex flex-col justify-center items-center gap-2 p-4 bg-[#171717] border border-[#262626] rounded-xl">
+                  <div className="flex justify-center items-center gap-2 w-full">
+                    {/* <div className="bg-[#0076DF] rounded-xl p-2 w-10 h-10 flex justify-center items-center">
+                      <i className="bi bi-clock text-xl"></i>
+                    </div> */}
+                    <p className="text-[#D5D3D3] font-bold">Press Data</p>
+                  </div>
+                  <p className="text-[#949A9C]">
+                    Get the parameters of the press used to <br /> mold this
+                    product
                   </p>
                 </div>
               </div>
